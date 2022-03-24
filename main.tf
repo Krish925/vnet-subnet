d0dae61ec31055be12a884b81d96301017650796
krishnakanth <91060972+Krish925@users.noreply.github.com>
2022-03-24 17:44:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 resource "azurerm_resource_group" "sample-aks" {
-  name     = "demo-aks"
-  location = "EastUS"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_network_security_group" "nsg" {
-  name                = "DemoNSG"
+  name                = var.network_security_group
   location            = azurerm_resource_group.sample-aks.location
   resource_group_name = azurerm_resource_group.sample-aks.name
 }
 
 resource "azurerm_virtual_network" "Vnet" {
-  name                = "DemoVnet"
+  name                = var.virtual_network_name
   location            = azurerm_resource_group.sample-aks.location
   resource_group_name = azurerm_resource_group.sample-aks.name
   address_space       = ["10.240.0.0/16"]
   dns_servers         = ["10.240.0.4", "10.240.0.5"]
 
   subnet {
-    name           = "subnet1"
+    name           = var.subnet
     address_prefix = "10.240.0.0/16"
   }
   tags = {
