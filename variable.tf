2b918ff53406e5b7cf2bb55c0934312791a4b253
krishnakanth <91060972+Krish925@users.noreply.github.com>
2022-03-24 17:46:32 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -14,20 +14,7 @@ variable "location" {
   type        = string
   description = "Resources location in Azure"
 }
-variable "cluster_name" {
+variable "network_security_group" {
   type        = string
-  description = "AKS name in Azure"
+  description = "security group name in Azure"
 }
-variable "kubernetes_version" {
-  type        = string
-  description = "Kubernetes version"
-}
-variable "system_node_count" {
-  type        = number
-  description = "Number of AKS worker nodes"
-}
-
-variable "vm_size" {
-  type        = string
-  description = "Name of VM Size"
-}    
